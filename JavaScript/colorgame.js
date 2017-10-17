7114a8c008d8004902f7c73a4fa6cea6a6796dbb
Zack <fanchekai@gmail.com>
2017-10-17 17:28:48 +0800

    color game

--- JavaScript/colorgame.js
+++ JavaScript/colorgame.js
@@ -1,105 +1,88 @@
+window.onload = function(){
+  initColor();
+
+}
+var rgb1,rgb2,rgb3;
+var success ;
+var refresh = document.querySelector('button');
+var subTitle = document.querySelector('h2');
+var content = document.querySelector('body');
+var footer = document.querySelector('span');
+
 
 function getRandomArbitrary(min, max){
-  return Math.random() * (max - min) + min;
+  return Math.floor(Math.random() * ( max - min ) + min); //interger number between 0~255
 }
 
-var r1, r2, r3, g1, g2, g3, b1, b2, b3 ;
-r1 = getRandomArbitrary(0, 255).toFixed(0);
-r2 = getRandomArbitrary(0, 255).toFixed(0);
-r3 = getRandomArbitrary(0, 255).toFixed(0);
-g1 = getRandomArbitrary(0, 255).toFixed(0);
-g2 = getRandomArbitrary(0, 255).toFixed(0);
-g3 = getRandomArbitrary(0, 255).toFixed(0);
-b1 = getRandomArbitrary(0, 255).toFixed(0);
-b2 = getRandomArbitrary(0, 255).toFixed(0);
-b3 = getRandomArbitrary(0, 255).toFixed(0);
-
-var rgb1 = 'rgb(' + r1 + ',' + g1 + ',' + b1 + ')';
-var rgb2 = 'rgb(' + r2 + ',' + g2 + ',' + b2 + ')';
-var rgb3 = 'rgb(' + r3 + ',' + g3 + ',' + b3 + ')';
-
-var h1 = document.querySelector('h1');
-h1.textContent = 'RGB(' + r1 + ',' + g1 + ',' + b1 + ')';
-
-var num = getRandomArbitrary(3, 1).toFixed(0);
-var img = 'img' + num;
-var img1 = document.getElementById(img);
-img1.style.backgroundColor = rgb1;
-
-var h2 = document.querySelector('h2');
-var body = document.querySelector('body');
-var span = document.querySelector('span');
-
-switch (6-num) {
-  case 3:
-    var img2 = document.getElementById('img1');
-    img2.style.backgroundColor = rgb2;
-    var img3 = document.getElementById('img2');
-    img3.style.backgroundColor = rgb3;
-    img1.onclick = function(){
-      h2.textContent = 'CORRECT!';
-      body.style.backgroundColor = rgb1;
-      img1.style.backgroundColor = 'white';
-      img2.style.backgroundColor = 'white';
-      img3.style.backgroundColor = 'white';
-      span.textContent = 'Play Again';
-    }
-    img2.onclick = function(){
-      h2.textContent = 'TRY AGAIN!';
-      img2.style.backgroundColor = '#212121';
-    }
-    img3.onclick = function(){
-      h2.textContent = 'TRY AGAIN!';
-      img3.style.backgroundColor = '#212121';
-    }
-    break;
-  case 4:
-    var img2 = document.getElementById('img1');
-    img2.style.backgroundColor = rgb2;
-    var img3 = document.getElementById('img3');
-    img3.style.backgroundColor = rgb3;
-    img1.onclick = function(){
-      h2.textContent = 'CORRECT!';
-      body.style.backgroundColor = rgb1;
-      img1.style.backgroundColor = 'white';
-      img2.style.backgroundColor = 'white';
-      img3.style.backgroundColor = 'white';
-      span.textContent = 'Play Again';
-    }
-    img2.onclick = function(){
-      h2.textContent = 'TRY AGAIN!';
-      img2.style.backgroundColor = '#212121';
-    }
-    img3.onclick = function(){
-      h2.textContent = 'TRY AGAIN!';
-      img3.style.backgroundColor = '#212121';
-    }
-    break;
-  case 5:
-    var img2 = document.getElementById('img2');
-    img2.style.backgroundColor = rgb2;
-    var img3 = document.getElementById('img3');
-    img3.style.backgroundColor = rgb3;
-    img1.onclick = function(){
-      h2.textContent = 'CORRECT!';
-      body.style.backgroundColor = rgb1;
-      img1.style.backgroundColor = 'white';
-      img2.style.backgroundColor = 'white';
-      img3.style.backgroundColor = 'white';
-      span.textContent = 'Play Again';
-    }
-    img2.onclick = function(){
-      h2.textContent = 'TRY AGAIN!';
-      img2.style.backgroundColor = '#212121';
-    }
-    img3.onclick = function(){
-      h2.textContent = 'TRY AGAIN!';
-      img3.style.backgroundColor = '#212121';
-    }
-    break;
+function randomColor(){
+  var r = getRandomArbitrary(0, 255); //set red color
+  var g = getRandomArbitrary(0, 255); //set green color
+  var b = getRandomArbitrary(0, 255); //set blue color
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
-var refresh = document.querySelector('button');
+function initColor(){
+  rgb1 = randomColor();
+  rgb2 = randomColor();
+  rgb3 = randomColor();
+  var title = document.querySelector('h1');
+  title.textContent = rgb1.toUpperCase();
+  checkStatus();
+}
+
+function checkStatus (){
+  var numberOfImage = getRandomArbitrary(3, 1);
+  var img = 'img' + numberOfImage;
+  var img1 = document.getElementById(img);
+  img1.style.backgroundColor = rgb1;
+
+  //based on first img position to deside other two.
+  switch (6-numberOfImage) {
+    case 3:
+      var img2 = document.getElementById('img1');
+      img2.style.backgroundColor = rgb2;
+      var img3 = document.getElementById('img2');
+      img3.style.backgroundColor = rgb3;
+      displayChange();
+      break;
+    case 4:
+      var img2 = document.getElementById('img1');
+      img2.style.backgroundColor = rgb2;
+      var img3 = document.getElementById('img3');
+      img3.style.backgroundColor = rgb3;
+      displayChange();
+      break;
+    case 5:
+      var img2 = document.getElementById('img2');
+      img2.style.backgroundColor = rgb2;
+      var img3 = document.getElementById('img3');
+      img3.style.backgroundColor = rgb3;
+      displayChange();
+      break;
+  }
+
+  // change the display after click
+  function displayChange(){
+    img1.onclick = function(){
+      subTitle.textContent = 'CORRECT!';
+      content.style.backgroundColor = rgb1;
+      img1.style.backgroundColor = 'white';
+      img2.style.backgroundColor = 'white';
+      img3.style.backgroundColor = 'white';
+      footer.textContent = 'Play Again';
+    }
+    img2.onclick = function(){
+      subTitle.textContent = 'TRY AGAIN!';
+      img2.style.backgroundColor = '#212121';
+    }
+    img3.onclick = function(){
+      subTitle.textContent = 'TRY AGAIN!';
+      img3.style.backgroundColor = '#212121';
+    }
+  }
+}
+
+//refresh the page;
 refresh.onclick = function(){
   window.location.reload(true);
 }
